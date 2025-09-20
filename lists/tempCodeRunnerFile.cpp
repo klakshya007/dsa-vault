eb8d96fb683e1e0a43ea7e0d6e583827a09e6b70
Lakshya <127542109+klakshya007@users.noreply.github.com>
2025-09-20 11:26:34 +0530

    Delete lists directory

--- lists/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-void DeletionI() {
-//     temp =head;
-//     int count,n = 0;
-//     cout <<"Enter The Node To Be Deleted : ";
-//     cin >>n;
-//     while (temp->next->next!=nullptr && count < n-1) {
-//         free(temp->next)
-//     }
-// }
